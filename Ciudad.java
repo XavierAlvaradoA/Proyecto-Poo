4489ca1614cbfba57c135307352f5ea6e912742f
XavierAlvaradoA <alvaradoaxavier1b@gmail.com>
2022-03-16 14:47:27 -0500

    Clase Ciudad

---
+++ Ciudad.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Ciudad {
+
+	
+	private String nombre;
+	private int[] consumo_city = new int[12];
+	
+	public Ciudad( String nombre) {
+		
+		this.nombre= nombre;
+		
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		
+	
+		return  " Ciudad: "+ nombre + " Consumo eléctrico: "  ;
+	}
+
+
+	
+}
